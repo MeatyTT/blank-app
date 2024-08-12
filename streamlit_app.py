9df30a6e56e37a8608f14ec8b137e474cccc6bf1
MeatyTT <63178475+MeatyTT@users.noreply.github.com>
2024-08-12 17:03:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-# )
+)
 # @st.cache_resource
 # def create_list():
 #     l = [1, 2, 3]
